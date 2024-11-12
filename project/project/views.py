0f7c34810810a1978062e3e609c178f37ce26ff1
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-13 01:10:57 +0600

    testing

--- project/project/views.py
+++ project/project/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from task import models
 def home(request):
     tasks=models.TaskModel.objects.all()
-
     for task in tasks:
          print(task.is_completed)
+         for category1 in task.category.all():
+              print(category1)
     return render(request,'home.html',{'tasks':tasks})
